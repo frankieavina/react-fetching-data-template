ab8b17b6b24cdf6df34c5c0dd17d936b8d8e4d8f
francisco avina <frankieavina@Frankies-MacBook-Pro.local>
2024-11-04 17:46:58 -0800

    Adding more to useQuery

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import logo from "./logo.png";
 import "./App.css";
 import pokemonData from "./pokemonapi.json";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   useQuery,
@@ -29,17 +29,20 @@ function App() {
 
   //----------------------------------Main Focus ------------------------------------//
   // const showPokemon = async (url) => {
-  //   //we are not handling the error or loading state in this example
-  //   //also the searchTerm isnt in sync with the response 
-  //   //ex. We are searching for a pokemon data 1 then we call for pokemon data 2. But sometimes
-  //   // what happens is we get the data for 2 first then 1. 
-  //   //SOOO... our state is out of sync with our fetch call !!!
+  // //   //we are not handling the error or loading state in this example
+  // //   //also the searchTerm isnt in sync with the response 
+  // //   //ex. We are searching for a pokemon data 1 then we call for pokemon data 2. But sometimes
+  // //   // what happens is we get the data for 2 first then 1. 
+  // //   //SOOO... our state is out of sync with our fetch call !!!
+
+  // //*************************************** original *******************************************/
   //   const response = await fetch(url);
   //   const data = await response.json();
   //   setSelectedPokemon(data);
+  // //*********************************************************************************************/
   // };
 
-  //This is what react docs recommend to fetch data
+ // This is what react docs recommend to fetch data
   // useEffect(() => {
   //   let ignore = false;
   //   setBio(null);
@@ -48,7 +51,7 @@ function App() {
   //       setBio(result);
   //     }
   //   });
-  //   if you return inside a useEffect its called as a clean up
+  //  // if you return inside a useEffect its called as a clean up
   //   return () => {
   //     ignore = true;
   //   };
@@ -106,21 +109,41 @@ function App() {
   //   },
   // })
 
+// seQuery take two parameters: Query key: A unique identifier for the query (in this case, 'pokemon').
+// Fetcher function: The function that performs the data fetching (in this case, fetchPosts).
+
+// States provided by useQuery:
+// data: Contains the API data if successfully fetched.
+// isLoading: A boolean that indicates whether the data is being fetched.
+// error: An object that contains any error that occurred during the fetch.
+
+  const fetchPokemon = async (url) => {
+    // Update the flag to trigger the query
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();  
+  };
+
   // Use the useQuery hook to fetch the data
   const { data, isLoading, error } = useQuery(
-    ['pokemon'], 
-    fetchPokemon,
+    ['pokemonData', selectedPokemon], 
+    () => fetchPokemon(selectedPokemon),
     {
-      onSuccess: (data) => {
-        // Save the data to useState when the query is successful
-        setSelectedPokemon(data);
-      }
+      enabled: !!selectedPokemon,
     }
   );
+
+  const handlePokemonSelect = (url) => {
+    setSelectedPokemon(url);
+  };
+
+
 // ********************************************************************************************************//
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>An error occurred: {error.message}</div>;
+  // if (isLoading) return <div>Loading...</div>;
+  // if (error) return <div>An error occurred: {error.message}</div>;
 
   return (
     <div className="App">
@@ -159,7 +182,7 @@ function App() {
         <ul>
           {filteredPokemonList.map((pokemon) => (
             <li key={pokemon.id} className="pokemon-item">
-              <a href="#" onClick={() => showPokemon(pokemon.url)}>
+              <a href="#" onClick={() => handlePokemonSelect(pokemon.url)}>
                 {pokemon.name}
               </a>
             </li>
